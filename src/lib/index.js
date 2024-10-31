a651a79145626d3219e02fdbdba6d363c13f7e2f
Jens Kleinhout <contact@hkdigital.nl>
2024-10-31 13:58:42 +0100

    Added exports stores, util, valibot

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,9 @@
 //
-// @note import from subfolders, e.g.
+// @note all exports are in subfolders
+//
+// @see package.json > exports
+//
+// @example
 // import { TimeStampSource } from '@hkdigital/lib-sveltekit-test/classes';
+//
 //
