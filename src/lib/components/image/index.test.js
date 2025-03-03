8319ad5300d89f3f3002dbade3a3050e5babbcbe
Jens Kleinhout <contact@hkdigital.nl>
2025-03-03 13:47:00 +0100

    Removed ResponsiveImage from unit test (replaced by ImageBox) Removed old files

--- src/lib/components/image/index.test.js
+++ src/lib/components/image/index.test.js
@@ -1,15 +1,9 @@
 import { describe, it, expect } from 'vitest';
 
-import { ImageBox, ResponsiveImage } from './index.js';
+import { ImageBox } from './index.js';
 
 describe('ImageBox', () => {
   it('should be a component (function)', () => {
     expect(typeof ImageBox).toBe('function');
-  });
-});
-
-describe('ResponsiveImage', () => {
-  it('should be a component (function)', () => {
-    expect(typeof ResponsiveImage).toBe('function');
   });
 });
