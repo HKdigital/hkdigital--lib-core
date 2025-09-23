e79d465ffce293702fe8592e3d80a10ca45dff6d
Jens Kleinhout <contact@hkdigital.nl>
2025-09-23 20:23:06 +0200

    services: added createGetService - creates getService with preset manager

--- src/lib/services/index.js
+++ src/lib/services/index.js
@@ -3,3 +3,33 @@ export { default as ServiceManager } from './service-manager/ServiceManager.js';
 
 export * from './service-base/constants.js';
 export * from './service-manager/constants.js';
+
+/**
+ * Create a getService function with a preset manager
+ *
+ * @param {import('./service-manager/ServiceManager.js').default} manager
+ *   ServiceManager instance to use
+ *
+ * @returns {<T>(serviceName: string) => T} getService function with preset manager
+ */
+export function createGetService(manager) {
+  /**
+   * Get a typed service instance from the preset manager
+   *
+   * @template T
+   * @param {string} serviceName - Name of the service to retrieve
+   *
+   * @returns {T} The service instance cast to the specified type
+   *
+   * @throws {Error} If service is not found
+   */
+  return function getService(serviceName) {
+    const service = manager.get(serviceName);
+
+    if (!service) {
+      throw new Error(`Service [${serviceName}] not found`);
+    }
+
+    return /** @type {T} */ (service);
+  };
+}
